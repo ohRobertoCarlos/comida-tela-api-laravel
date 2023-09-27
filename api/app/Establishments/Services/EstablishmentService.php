bd2933cc5855213d58f23ad01f6c8e1aa2daa918
Roberto Carlos <robertocarlosdeveloper@gmail.com>
2023-09-27 08:35:35 -0300

    generate qrcode with host of app client (front-end)

--- api/app/Establishments/Services/EstablishmentService.php
+++ api/app/Establishments/Services/EstablishmentService.php
@@ -69,7 +69,9 @@ class EstablishmentService
 
     public function generateQrCodeMenu(BaseModel $establishment) : string
     {
-        $base64 = (new QRCode())->render(url('/' . $establishment->menu_code));
+        $base64 = (new QRCode())->render(
+            env('APP_CLIENT_URL', 'http://localhost:3000') . '/' . $establishment->menu_code
+        );
         $base64 = str_replace('data:image/png;base64,', '', $base64);
         $base64 = str_replace(' ', '+', $base64);
 
