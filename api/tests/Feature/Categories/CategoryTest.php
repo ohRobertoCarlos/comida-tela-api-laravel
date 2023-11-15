c8cf9b09686b679b3b222f844ae320dae2eaf483
Roberto Carlos <robertocarlosdeveloper@gmail.com>
2023-11-15 17:02:51 -0300

    delete category tests added

--- api/tests/Feature/Categories/CategoryTest.php
+++ api/tests/Feature/Categories/CategoryTest.php
@@ -70,3 +70,53 @@ test('establishment user must update an establishment category', function () {
     $this->assertEquals('Orange Cakes', $category->name);
 });
 
+test('Admin user cannot delete an establishment category', function () {
+    $establishment = createEstablishment();
+    $token = getTokenUserAdminLogged();
+
+    $category = createCategoryEstablishment($establishment->id);
+
+    $reponse = withHeaders([
+        'accept' => 'application/json',
+        'Authorization' => 'Bearer ' . $token,
+    ])->delete('/api/v1/establishments/' . $establishment->id . '/categories/' . $category->id);
+
+    $reponse->assertForbidden();
+});
+
+test('establishment user must delete an establishment category', function () {
+    $establishment = createEstablishment();
+    $token = getTokenUserEstablishmentLogged($establishment->id);
+
+    $category = createCategoryEstablishment($establishment->id);
+
+    $reponse = withHeaders([
+        'accept' => 'application/json',
+        'Authorization' => 'Bearer ' . $token,
+    ])->delete('/api/v1/establishments/' . $establishment->id . '/categories/' . $category->id);
+
+    $reponse->assertOk();
+
+    $category = $category->fresh();
+
+    $this->assertTrue(!empty($category->deleted_at));
+});
+
+test('establishment user must not delete an establishment category with items', function () {
+    $reponse = createEstablishmentWithMenu();
+    $reponse->assertCreated();
+
+    $token = getTokenUserEstablishmentLogged($reponse->json('data.id'));
+
+    $category = createCategoryEstablishment($reponse->json('data.id'));
+    $item = createItemMenu(['menu_id' => $reponse->json('data.menu.id')]);
+
+    $item->categories()->sync([$category->id]);
+
+    $reponse = withHeaders([
+        'accept' => 'application/json',
+        'Authorization' => 'Bearer ' . $token,
+    ])->delete('/api/v1/establishments/' . $reponse->json('data.id') . '/categories/' . $category->id);
+
+    $reponse->assertBadRequest();
+});
