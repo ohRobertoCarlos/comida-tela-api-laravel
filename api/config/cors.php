cd127ff4395b2d05a763f0c25913d9dbb99dc373
Roberto Carlos <robertocarlosdeveloper@gmail.com>
2024-09-01 10:49:06 -0300

    add alloed origin cors config

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [trim(env('APP_CLIENT_URL', ''), '/')],
 
     'allowed_origins_patterns' => [],
 
