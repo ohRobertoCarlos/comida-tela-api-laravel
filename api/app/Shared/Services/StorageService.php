9f5c33241eee1610777d40182bce8d5f9bcde655
Roberto Carlos <robertocarlosdeveloper@gmail.com>
2024-09-02 17:19:54 -0300

    fix url location public images

--- api/app/Shared/Services/StorageService.php
+++ api/app/Shared/Services/StorageService.php
@@ -8,6 +8,10 @@ class StorageService
 {
     public static function getUrlPublicFile(string $publicFilePath) : string
     {
+        if (filter_var($publicFilePath, FILTER_VALIDATE_URL) !== false) {
+            return $publicFilePath;
+        }
+
         return Storage::disk(env('PUBLIC_FILESYSTEM_DISK'))->url($publicFilePath);
     }
 }
